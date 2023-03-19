f3914f7812773b8737def5ca1b0a69fb989d8d49
Anwesh <anweshknayak@gmail.com>
2023-03-20 01:21:21 +0530

    5. block impl added

---
+++ src/5-blocks/1-events.ts
@@ -0,0 +1,47 @@
+// needed as of 7.x series, see CHANGELOG of the api repo.
+import '@polkadot/api-augment';
+import '@polkadot/types-augment';
+
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { formatBalance } from '@polkadot/util';
+import { toUnit } from '../utils/unitConversions';
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+/**
+ *  TODO: add your block hash here
+ */
+const blockHash = '0x43376170b4c1c446e0506c5d39b70a88d68795a0c21002980d70f29866888671';
+
+
+async function main() {
+  const wsProvider = new WsProvider(process.env.WS_URL);
+  // Create a new instance of the api
+  const api = await ApiPromise.create({ provider: wsProvider, noInitWarn: true });
+
+  /**
+   * 
+   * 1. Retrieve the block hash of the block and fetch extrinsics and events
+   *    
+   */
+
+  // Returns a decorated API instance at a specific block hash
+  const apiAt = await api.at(blockHash);
+  // Get the events for the block hash
+  const events = await apiAt.query.system.events();
+  // Get the block for the block hash
+  const signedBlock = await api.rpc.chain.getBlock(blockHash);
+  // Get the extrinsics for the block hash 
+  signedBlock.block.extrinsics.forEach(({ method: { method, section } }, index) => {
+
+    console.log(`\n Extrinsic ${index} is ${section}.${method}`);
+    events
+      .forEach(({ event }) => {
+        console.log(`\n Event ${event.section}.${event.method}:: ${JSON.stringify(event.data)}`);
+
+      });
+
+  });
+}
+
+main().catch(console.error);
